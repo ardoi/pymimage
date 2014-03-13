e06ae3c126cba502ea7494922a066c3ddd9c4158
Ardo Illaste <ardo.illaste@gmail.com>
2014-03-13 16:22:26 +0100

    Removed named images Added logging Commented out OME-TIFF reading

--- pymimage/readers/OMEXMLreader.py
+++ pymimage/readers/OMEXMLreader.py
@@ -5,6 +5,7 @@ import StringIO
 import zlib
 import datetime
 import os
+import logging
 
 import numpy
 
@@ -26,54 +27,60 @@ class OMEXMLReader(object):
 
     def __init__(self, filename):
         self.filename = filename
-
+        self.image_attrs = {}
+        self.images = {}
+        self.active_image_number = None
+        self.logger = logging.getLogger(__name__)
+    @property
+    def pixels(self):
+        return self.image_width*self.image_height
     @property
     def image_width(self):
-        return self.image_attrs[self.active_type]["image_width"]
+        return self.image_attrs[self.active_image_number]["image_width"]
 
     @image_width.setter
     def image_width(self, val ):
-        self.image_attrs[self.active_type]["image_width"] = val
+        self.image_attrs[self.active_image_number]["image_width"] = val
 
     @property
     def image_height(self):
-        return self.image_attrs[self.active_type]["image_height"]
+        return self.image_attrs[self.active_image_number]["image_height"]
 
     @image_height.setter
     def image_height(self, val ):
-        self.image_attrs[self.active_type]["image_height"] = val
+        self.image_attrs[self.active_image_number]["image_height"] = val
 
     @property
     def channels(self):
-        return self.image_attrs[self.active_type]["channels"]
+        return self.image_attrs[self.active_image_number]["channels"]
 
     @channels.setter
     def channels(self, val ):
-        self.image_attrs[self.active_type]["channels"] = val
+        self.image_attrs[self.active_image_number]["channels"] = val
 
     @property
     def frames(self):
-        return self.image_attrs[self.active_type]["frames"]
+        return self.image_attrs[self.active_image_number]["frames"]
 
     @frames.setter
     def frames(self, val ):
-        self.image_attrs[self.active_type]["frames"] = val
+        self.image_attrs[self.active_image_number]["frames"] = val
 
     @property
     def data_type(self):
-        return self.image_attrs[self.active_type]["data_type"]
+        return self.image_attrs[self.active_image_number]["data_type"]
 
     @data_type.setter
     def data_type(self, val ):
-        self.image_attrs[self.active_type]["data_type"] = val
+        self.image_attrs[self.active_image_number]["data_type"] = val
 
     @property
     def image_step_y(self):
-        return self.image_attrs[self.active_type]["image_step_y"]
+        return self.image_attrs[self.active_image_number]["image_step_y"]
 
     @image_step_y.setter
     def image_step_y(self, val ):
-        self.image_attrs[self.active_type]["image_step_y"] = val
+        self.image_attrs[self.active_image_number]["image_step_y"] = val
 
     def read_meta(self):
         self.ome_type = os.path.splitext(self.filename)[-1]
@@ -131,51 +138,53 @@ class OMEXMLReader(object):
         microscope before a linescan). Linescan image is used for the general image attributes as those are the ones
         relevant to analysis.
         """
-        print "\nImage attrib"
-        self.active_type = None
-        self.image_attrs = {"Pixels": {}, "Reference": {}}
-        images = {"Pixels": None, "Reference": None}
+        self.logger.info("Reading image attributes for image {}".format(self.filename))
         image_elements = self.et.findall(self.fulltags["Image"])
         for im_n, image_element in enumerate(image_elements):
             pixels = image_element.findall(self.fulltags["Pixels"])
-            #channel_dict = {}
-            tiffdata_dict = {}
+            bindata_dict = {}
             image_stuff = {"Attributes": image_element.attrib,# "Channels": channel_dict,
-                    "BinDatas": tiffdata_dict, "ImageData": None}
+                    "BinDatas": bindata_dict, "ImageData": None}
             for pi_n, pixel in enumerate(pixels):
+                #There is only one pixel element per Image (although the schema allows more)
+                #TODO In the next OME release Pixel data will be moved up to Image
+
                 #channels = pixel.findall(self.fulltags["Channel"])
                 #for ch_n, channel in enumerate(channels):
                 #    channel_dict[ch_n] = channel
-                tiffdatas = pixel.findall(self.fulltags[self.bintagname])
-                for bin_n, tiffdata in enumerate(tiffdatas):
+                bindatas = pixel.findall(self.fulltags[self.bintagname])
+                for bin_n, bindata in enumerate(bindatas):
                     if self.bintagname == "BinData":
-                        if int(tiffdata.attrib["Length"]) > 0:
-                            tiffdata_dict[bin_n] = tiffdata
-                    else:
-                        tiffdata_dict[bin_n] = tiffdata
+                        if int(bindata.attrib["Length"]) > 0:
+                            bindata_dict[bin_n] = bindata
+                    #else:
+                    #    tiffdata_dict[bin_n] = tiffdata
                 image_stuff["PixelAttributes"] = pixel.attrib
-                if pixel.attrib["SizeX"] == pixel.attrib["SizeY"] and int(pixel.attrib['SizeT'])==1:
-                    #assume that a square image is the reference image
-                    #FIXME
-                    image_type = "Reference"
-                else:
-                    image_type = "Pixels"
-                break #because there should only be one Pixel element in an Image tag
-            images[image_type] = image_stuff
-            self.active_type = image_type
-            pix_attr = images[image_type]["PixelAttributes"]
+                self.image_attrs[im_n]={}
+            self.images[im_n] = image_stuff
+            self.active_image_number = im_n
+            #we can do this because we know only one Pixel element exists per Image
+            pix_attr = self.images[self.active_image_number]["PixelAttributes"]
             self.image_width = int(pix_attr['SizeX'])
             self.image_height = int(pix_attr['SizeY'])
             self.channels = int(pix_attr['SizeC'])
             self.frames = int(pix_attr['SizeT'])
             self.data_type = pix_attr["Type"]
             try:
-                self.image_step_y = float(pix_attr['PhysicalSizeX'])
+                self.image_step_x = float(pix_attr['PhysicalSizeX'])
             except KeyError:
-                self.image_step_y = 1.0
-        #pprint(images)
-        self.images = images
-        self.image_name = images["Pixels"]["Attributes"]["Name"]
+                self.image_step_x = None
+            try:
+                self.image_step_y = float(pix_attr['PhysicalSizeY'])
+            except KeyError:
+                self.image_step_y = None
+            message = "Image {}: {} x {} pixels ({} total), {} channels, {} frames".\
+                    format(self.active_image_number, self.image_width, self.image_height,
+                           self.pixels, self.channels, self.frames)
+            if self.image_step_x and self.image_step_y:
+                message+=", dx:dy = {} : {}".format(self.image_step_x, self.image_step_y)
+            self.logger.info(message)
+        self.image_name = self.images[0]["Attributes"]["Name"]
         date = image_elements[0].find(self.fulltags["AcquisitionDate"]).text
         self.datetime = datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%S')
         #print "attributes=",self.image_attrs
@@ -185,24 +194,24 @@ class OMEXMLReader(object):
         except AttributeError:
             self.description = ""
             pass
-        self.active_type = "Pixels"
+        self.active_image_number = 0
         self._get_extra_info()
-        if images["Reference"]:
-            #try to get ROI
-            roi_element = self.et.find(self.fulltags["ROI/Union/Shape/Line"])
-            #print "ROI element", roi_element is None
-            if roi_element is not None:
-                roi_attrib = roi_element.attrib
-                #print roi_attrib
-                roi = {"x2":float(roi_attrib["X2"]),"x1":float(roi_attrib["X1"]),
-                        "y2":float(roi_attrib["Y2"]), "y1":float(roi_attrib["Y1"])}
-                images["Reference"]["ROI"] = roi
-            else:
-                images["Reference"]["ROI"] = None
+        #if images["Reference"]:
+        #    #try to get ROI
+        #    roi_element = self.et.find(self.fulltags["ROI/Union/Shape/Line"])
+        #    #print "ROI element", roi_element is None
+        #    if roi_element is not None:
+        #        roi_attrib = roi_element.attrib
+        #        #print roi_attrib
+        #        roi = {"x2":float(roi_attrib["X2"]),"x1":float(roi_attrib["X1"]),
+        #                "y2":float(roi_attrib["Y2"]), "y1":float(roi_attrib["Y1"])}
+        #        images["Reference"]["ROI"] = roi
+        #    else:
+        #        images["Reference"]["ROI"] = None
 
 
     def read_image(self, image_type):
-        self.active_type = image_type
+        self.active_image_number = image_type
         if not self.metadata_loaded:
             self.read_meta()
         if self.images[image_type]:
@@ -212,7 +221,7 @@ class OMEXMLReader(object):
 
 
     def _get_image_data(self, image_type):
-        #print "reading data for type %s"%self.active_type
+        self.logger.info("Reading data for image {}".format(self.active_image_number))
         tiffdata_elements = self.images[image_type]["BinDatas"]
         data_array = numpy.zeros(shape=(self.channels, self.frames, self.image_height, self.image_width),
                 dtype=self.data_type)
@@ -249,13 +258,10 @@ class OMEXMLReader(object):
                             dtype).astype('float32')
                 else:
                     image_data = numpy.fromstring(stringio_out.getvalue(), dtype).astype('float32')
-                #print 'im',image_data
-            elif self.bintagname == "TiffData":
-                ifd = int(bin_attrib["IFD"])
-                #print ifd
-                #print self.pil_image
-                self.pil_image.seek(ifd)
-                image_data = numpy.array(self.pil_image.getdata(),'float')
+            #elif self.bintagname == "TiffData":
+            #    ifd = int(bin_attrib["IFD"])
+            #    self.pil_image.seek(ifd)
+            #    image_data = numpy.array(self.pil_image.getdata(),'float')
             #Need to read image dimension from PixelAttribute as they are different for different image types
             image_width = int(self.images[image_type]["PixelAttributes"]["SizeX"])
             image_height = int(self.images[image_type]["PixelAttributes"]["SizeY"])
