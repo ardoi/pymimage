e49cb4b1717b7beaf83e51e55f97da6a495f53b6
Ardo Illaste <ardo.illaste@gmail.com>
2014-03-17 16:58:08 +0100

    Fixes to tests

--- test/test_reading.py
+++ test/test_reading.py
@@ -12,13 +12,10 @@ class TestOIBConversion():
 
     @classmethod
     def setup_class(cls):
-        "create tmp"
-        if not os.path.isdir("tmp"):
-            os.mkdir("tmp")
-        return
         "Remove existing file"
         ome_name = os.path.join("tmp", "e06250156e.ome")
         try:
+            pass
             os.remove(ome_name)
         except OSError:
             pass
@@ -30,12 +27,18 @@ class TestOIBConversion():
         self.ome_file = self.imaker.load_file(self.filename)
 
     def test_load(self):
+        """test"""
+        assert self.ome_file is not None
         assert self.ome_file.filename == self.ome_name
 
     def test_attr(self):
+        """The OIB file has two images. Test whether this is true and
+        whether their parameters are what they should be. Since image
+        has not been read then the ImageData for each image should
+        be None"""
+
         attrib = self.ome_file.image_attrs
         assert 1 in attrib
-        assert 0 in attrib
         assert attrib[0]['frames'] == 1
         assert attrib[0]['channels'] == 1
         assert attrib[0]['image_height'] == 10000
@@ -44,6 +47,7 @@ class TestOIBConversion():
         assert attrib[0]['image_step_x'] == 0.276
         assert self.ome_file.images[0]['ImageData'] is None
 
+        assert 0 in attrib
         assert attrib[1]['frames'] == 1
         assert attrib[1]['channels'] == 1
         assert attrib[1]['image_height'] == 512
@@ -53,22 +57,18 @@ class TestOIBConversion():
         assert self.ome_file.images[0]['ImageData'] is None
 
     def test_data(self):
+        """Read image data for both images and check if dimensions
+        match what is expected"""
         shapes = ((1, 1, 10000, 344), (1, 1, 512, 512))
         for i in [0, 1]:
             self.ome_file.read_image(i)
             im_data = self.ome_file.images[i]['ImageData']
             im_shape = im_data.shape
             assert im_shape == shapes[i]
+            assert im_data.mean() > 0
 
 
 class TestMissingFileConversion():
-
-    @classmethod
-    def setup_class(cls):
-        "create tmp"
-        if not os.path.isdir("tmp"):
-            os.mkdir("tmp")
-        return
 
     def setup(self):
         self.filename = os.path.join("test", "data", "nosuchfile.oib")
@@ -80,13 +80,6 @@ class TestMissingFileConversion():
 
 
 class TestInvalidFileConversion():
-
-    @classmethod
-    def setup_class(cls):
-        "create tmp"
-        if not os.path.isdir("tmp"):
-            os.mkdir("tmp")
-        return
 
     def setup(self):
         self.filename = os.path.join("test", "data", "invalid.oib")
