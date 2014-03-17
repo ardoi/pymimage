e053fdef6f2cb7e316e7bc65007c56b6a6bfde16
Ardo Illaste <ardo.illaste@gmail.com>
2014-03-17 16:38:07 +0100

    New version of bftools (Version: 5.1.0-DEV  VCS revision: c558413 Build date: 14 March 2014)

--- bftools/ijview.bat
+++ bftools/ijview.bat
@@ -3,50 +3,41 @@
 rem ijview.bat: a batch file for displaying an image file in ImageJ
 rem             using the Bio-Formats Importer plugin
 
-rem Required JARs: loci_tools.jar, ij.jar
+rem Required JARs: loci_tools.jar or bioformats_package.jar, ij.jar
 
-rem JAR libraries must be in the same directory as this
-rem command line script for the command to function.
+setlocal
+set BF_DIR=%~dp0
+if "%BF_DIR:~-1%" == "\" set BF_DIR=%BF_DIR:~0,-1%
 
-rem If you are a developer working from source and have
-rem the LOCI classes in your CLASSPATH, you can set the
-rem LOCI_DEVEL environment variable to use them instead.
+call "%BF_DIR%\config.bat"
 
-set PROG=loci.plugins.in.Importer
-set DIR=%~dp0
-if "%DIR:~1%" == ":\" (
-  set DIR1=%DIR%
-) else (
-  rem Remove trailing backslash
-  set DIR1=%DIR:~0,-1%
+if "%BF_DEVEL%" == "" (
+  rem Developer environment variable unset; add JAR libraries to classpath.
+  if exist "%BF_JAR_DIR%\ij.jar" (
+    set BF_CP="%BF_JAR_DIR%\ij.jar"
+  ) else (
+    rem Libraries not found; issue an error.
+    echo Required JAR libraries not found. Please download:
+    echo   ij.jar
+    echo from:
+    echo   http://imagej.nih.gov/ij/upgrade/
+    echo and place in the same directory as the command line tools.
+    goto end
+  )
+  if exist "%BF_JAR_DIR%\bio-formats_plugins.jar" (
+    set BF_CP=%BF_CP%;"%BF_JAR_DIR%\bio-formats_plugins.jar"
+  ) else if not exist "%BF_JAR_DIR%\bioformats_package.jar" (
+    rem Libraries not found; issue an error.
+    echo Required JAR libraries not found. Please download:
+    echo   bioformats_package.jar
+    echo from:
+    echo   http://www.openmicroscopy.org/site/products/bio-formats/downloads
+    echo and place in the same directory as the command line tools.
+    goto end
+  )
 )
 
-if "%LOCI_DEVEL%" == "" (
-  rem Developer environment variable unset; look for proper libraries
-  if not exist "%DIR%ij.jar" goto missing
-  if exist "%DIR%loci_tools.jar" goto found
-  if exist "%DIR%bio-formats.jar" goto found
-  goto missing
-) else (
-  rem Developer environment variable set; try to launch
-  java -mx512m %PROG% %*
-  goto end
-)
-
-:found
-rem Library found; try to launch
-java -mx512m -cp "%DIR1%";"%DIR%bio-formats.jar";"%DIR%loci_tools.jar";"%DIR%ij.jar" %PROG% %*
-goto end
-
-:missing
-echo Required JAR libraries not found. Please download:
-echo   loci_tools.jar
-echo from:
-echo   http://www.loci.wisc.edu/bio-formats/downloads
-echo and:
-echo   ij.jar
-echo from:
-echo   http://rsb.info.nih.gov/ij/upgrade/
-echo and place in the same directory as the command line tools.
+set BF_PROG=loci.plugins.in.Importer
+call "%BF_DIR%\bf.bat" %*
 
 :end
