dc7612225d6ac43419ab68aa734cceda117a1fe5
Ardo Illaste <ardo.illaste@gmail.com>
2014-03-17 15:35:46 +0100

    Whitespace and ignore file

--- pymimage/readers/LSMreader.py
+++ pymimage/readers/LSMreader.py
@@ -5,6 +5,7 @@ import numpy
 from OMEXMLreader import OMEXMLReader
 from customreader import CustomReader
 
+
 class LSMReader(OMEXMLReader, CustomReader):
     ftype = "lsm"
 
@@ -14,33 +15,36 @@ class LSMReader(OMEXMLReader, CustomReader):
         raw_timestamps = {}
         raw_events = {}
         for key in raw_keys:
-            #collect events
+            # collect events
             if 'Event' in key:
                 match = re.match("Event(?P<number>\d+)\s(?P<type>\w+)", key)
                 if match:
                     event_number = int(match.group('number'))
                     ann_type = match.group('type')
                     if event_number in raw_events:
-                        raw_events[event_number].update({ann_type: self.raw_annotation[key].strip()})
+                        raw_events[event_number].update(
+                            {ann_type: self.raw_annotation[key].strip()})
                     else:
-                        raw_events.update({event_number: {ann_type: self.raw_annotation[key].strip()}})
-            #collect timestamps
+                        raw_events.update(
+                            {event_number: {ann_type:
+                                            self.raw_annotation[key].strip()}})
+            # collect timestamps
             elif 'TimeStamp' in key:
                 match = re.match("TimeStamp(?P<number>\d+)", key)
                 if match:
                     timestamp_group_number = int(match.group('number'))
                     raw_timestamps.update(
-                        {int(timestamp_group_number): numpy.fromstring(self.raw_annotation[key], sep=',')})
+                        {int(timestamp_group_number):
+                         numpy.fromstring(self.raw_annotation[key], sep=',')})
             elif 'Notes' in key:
                 match = re.match("Recording #\d+ Notes", key)
                 if match:
                     self.notes = self.raw_annotation[key]
             else:
                 pass
-                #print key,raw_annotation[key]
+                # print key,raw_annotation[key]
 
-
-        #combine timestamps
+        # combine timestamps
         self.event_times = []
         for event_no in raw_events.keys():
             self.event_times.append(float(raw_events[event_no]['Time']))
@@ -49,15 +53,13 @@ class LSMReader(OMEXMLReader, CustomReader):
         timestamp_keys = raw_timestamps.keys()
         timestamp_keys.sort()
         timestamp_list = [raw_timestamps[ts] for ts in timestamp_keys]
-        #print raw_timestamps,timestamp_keys
+        # print raw_timestamps,timestamp_keys
         if timestamp_list:
-            self.timestamps = numpy.hstack(timestamp_list)*1000#timestamps in seconds, we want ms
+            # timestamps in seconds, we want ms
+            self.timestamps = numpy.hstack(timestamp_list) * 1000
             #v = helpers.find_outliers(s.diff(s.array(self.timestamps)))
             v = numpy.diff(numpy.array(self.timestamps))
             self.interval = v[1]
         else:
             self.timestamps = None
             self.interval = None
-
-
-
